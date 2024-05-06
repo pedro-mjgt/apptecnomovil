8e12afac08871b2bc2760d152b3adf0605bf593a
pedro-mjgt <pedro.mjgt@gmail.com>
2024-05-06 18:24:34 -0500

    initial commit

---
+++ src/main/java/com/pe/cibertec/apptecnomovil/ApptecnomovilApplication.java
@@ -0,0 +1,13 @@
+package com.pe.cibertec.apptecnomovil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApptecnomovilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApptecnomovilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pe/cibertec/apptecnomovil/ApptecnomovilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pe.cibertec.apptecnomovil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApptecnomovilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
